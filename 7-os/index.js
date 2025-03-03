c1d3c3eaeb9ca5def4a256333cb5f0c7a955e2ba
mahbubul-shafi <mahbubul.shafi@gmail.com>
2025-03-04 01:22:37 +0600

    learned os and path

---
+++ 7-os/index.js
@@ -0,0 +1,17 @@
+// const os = require("os")
+// const { totalmem } = require("os")
+// console.log(os)
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.hostname());
+
+// console.log(totalmem());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const path = require("path")
+// console.log(path);
+// console.log(path.extname("index.html"))
+
+console.log(path.join(__dirname + "/../views"));
